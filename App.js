3db48c91fcefb6f24e40d47e19458f66e15ce12d
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2019-11-28 12:14:43 +0200

    Channels: Change channels and start from first unwatched video

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
@@ -11,6 +12,13 @@ import React, {useState, useEffect} from 'react';
 import YouTube from 'react-native-youtube';
 import channelData from './src/data/channels';
 import ApiKeys from './api_keys';
+import {
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  DrawerLayoutAndroid,
+} from 'react-native';
 
 const VIDEO_STATES = {
   ended: 'ended',
@@ -25,6 +33,22 @@ const App = () => {
     VIDEO_STATES.stopped,
   );
   const [hidden, setHidden] = useState(false);
+
+  const updateChannelsFor = channel => {
+    let updateChannels = channels;
+
+    let newChannelIndex = updateChannels.findIndex(updateChannel => updateChannel.id === channel.id);
+
+    updateChannels[newChannelIndex] = channel;
+    setChannels(updateChannels);
+  };
+
+  const changeChannel = channel => {
+    setHidden(true);
+    setCurrentChannel(channel);
+    setCurrentVideoState(VIDEO_STATES.stopped);
+    updateChannelsFor(channel);
+  };
 
   useEffect(() => {
     setChannels(channelData);
@@ -41,32 +65,65 @@ const App = () => {
       updatePlaylist = updatePlaylist.concat(previousVideo);
       updateChannel.playlist = updatePlaylist;
 
-      setHidden(true);
-      setCurrentChannel(updateChannel);
-      setCurrentVideoState(VIDEO_STATES.stopped);
+      changeChannel(updateChannel);
     }
-  }, [currentVideoState, currentChannel]);
+  }, [currentVideoState]);
 
   useEffect(() => {
     if (hidden) {
       setCurrentVideo(currentChannel.playlist[0]);
       setHidden(false);
     }
-  }, [hidden, currentChannel]);
+  }, [hidden]);
 
   return (
-    <>
+    <DrawerLayoutAndroid
+      drawerWidth={300}
+      drawerPosition={'left'}
+      renderNavigationView={() => (
+        <FlatList
+          data={channels}
+          showsVerticalScrollIndicator={false}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => changeChannel(item)}>
+                <Text style={styles.item}>{item.name}</Text>
+              </TouchableOpacity>
+            );
+          }}
+          keyExtractor={item => `${item.id}`}
+        />
+      )}>
       {hidden ? null : (
         <YouTube
           apiKey={ApiKeys.YOUTUBE_API_KEY}
           videoId={currentVideo}
-          play={true}
+          play
           onChangeState={e => setCurrentVideoState(e.state)}
           style={{alignSelf: 'stretch', height: 300}}
         />
       )}
-    </>
+    </DrawerLayoutAndroid>
   );
 };
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+});
 
 export default App;
